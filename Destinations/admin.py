95fff42a69f7955713e006f4f79360ef66bbe756
Pramod Gopinath <gopinath.pramod@gmail.com>
2024-01-23 20:29:48 +0530

    Admin Feachers

--- Destinations/admin.py
+++ Destinations/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Destination, Packages, Journey, Contry
+from .models import Destination, Packages, Journey, Contry, TourGuids, TourGuideBooking, HotelBooking, PackageBooking, JournyBooking
 
 admin.site.register(Destination)
 admin.site.register(Packages)
 admin.site.register(Journey)
 admin.site.register(Contry)
+admin.site.register(TourGuids)
+admin.site.register(TourGuideBooking)
+admin.site.register(HotelBooking)
+admin.site.register(PackageBooking)
+admin.site.register(JournyBooking)
+
